d8a7b6dfd3cf6fe8c2206522d99ac77ba5103465
Jayesh Varu <varu.jayesh7@gmail.com>
2021-05-22 15:03:24 +0530

    added auth system

--- routes/web.js
+++ routes/web.js
@@ -1,22 +1,22 @@
 const homeController = require('../app/http/controllers/homeController')
-// const authController = require('../app/http/controllers/authController')
+const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
 // const orderController = require('../app/http/controllers/customers/orderController')
 // const adminOrderController = require('../app/http/controllers/admin/orderController')
 // const statusController = require('../app/http/controllers/admin/statusController')
 
 // Middlewares 
-// const guest = require('../app/http/middlewares/guest')
+const guest = require('../app/http/middlewares/guest')
 // const auth = require('../app/http/middlewares/auth')
 // const admin = require('../app/http/middlewares/admin')
 
 function initRoutes(app) {
     app.get('/', homeController().index)
-    // app.get('/login', guest, authController().login)
-    // app.post('/login', authController().postLogin)
-    // app.get('/register', guest, authController().register)
-    // app.post('/register', authController().postRegister)
-    // app.post('/logout', authController().logout)
+    app.get('/login', guest, authController().login)
+    app.post('/login', authController().postLogin)
+    app.get('/register', guest, authController().register)
+    app.post('/register', authController().postRegister)
+    app.post('/logout', authController().logout)
 
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('express-flash')
 const MongoDbStore  = require('connect-mongo');
-
+const passport = require('passport')
 
 // Database connection
 // const url = 'mongodb://localhost/pizza';
@@ -37,6 +37,12 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 //Assets
 app.use(express.static('public'))
@@ -47,7 +53,7 @@ app.use(express.json())
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
-    // res.locals.user = req.user
+    res.locals.user = req.user
     next()
 })
 
