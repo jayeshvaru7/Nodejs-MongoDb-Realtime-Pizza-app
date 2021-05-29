3697dbcf4b83f80ab7a8dab09dc0888acd3e3537
Jayesh Varu <varu.jayesh7@gmail.com>
2021-05-29 15:13:41 +0530

    modified server js

--- server.js
+++ server.js
@@ -1,95 +1,24 @@
-require('dotenv').config()
-
-const express  = require('express');
-
+const express = require('express');
 const app = express();
-const ejs = require('ejs');
 const path = require('path');
-const expressLayout = require('express-ejs-layouts');
-const PORT = process.env.PORT || 3000;
-const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('express-flash')
-const MongoDbStore  = require('connect-mongo');
-const passport = require('passport')
-const Emitter = require('events')
+const port = process.env.PORT || 3000;
 
-// Database connection
-// const url = 'mongodb://localhost/pizza';
-// const url = 'mongodb+srv://mongoAdmin:saymongocheese@cluster0.lh4ly.mongodb.net/pizza';
+const urlShortener = require('node-url-shortener');
 
-mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database connected...');
-}).catch(err => {
-    console.log('Connection failed...')
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// Event emitter
-const eventEmitter = new Emitter()
-app.set('eventEmitter', eventEmitter)
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
 
-// Session config
-app.use(session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    store: MongoDbStore .create({
-        mongoUrl: process.env.MONGO_CONNECTION_URL,
-        collectionName: 'sessions'
-    }),
-    saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
-}))
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.urlencoded());
 
-// Passport config
-const passportInit = require('./app/config/passport')
-passportInit(passport)
-app.use(passport.initialize())
-app.use(passport.session())
+app.post('/url', function(req, res) {
+    const url = req.body.url;
 
-app.use(flash())
-//Assets
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-// Global middleware
-app.use((req, res, next) => {
-    res.locals.session = req.session
-    res.locals.user = req.user
-    next()
-})
-
-//set Template engine
-app.use(expressLayout)
-app.set('views',path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
-
-
-require('./routes/web')(app)
-app.use((req, res) => {
-    res.status(404).render('errors/404')
-})
-
-const server = app.listen(PORT , () => {
-    console.log(`Listening on port ${PORT}`)
-})
-
-// Socket
-
-const io = require('socket.io')(server)
-io.on('connection', (socket) => {
-// Join
-socket.on('join', (roomName) => {
-socket.join(roomName)
-})
-})
-
-eventEmitter.on('orderUpdated', (data) => {
-    io.to(`order_${data.id}`).emit('orderUpdated', data)
-})
-
-eventEmitter.on('orderPlaced', (data) => {
-    io.to('adminRoom').emit('orderPlaced', data)
-})
+    urlShortener.short(url, function(err, shortUrl){
+        res.send(shortUrl);
+    });
+});
